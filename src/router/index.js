e129dcfc3aa338591141a4dfdef15e65629dbac9
zhengguiping <907070621@qq.com>
2020-04-12 21:35:59 +0800

    树形

--- src/router/index.js
+++ src/router/index.js
@@ -72,7 +72,7 @@ export const constantRoutes = [
         path: 'tree',
         name: 'Tree',
         component: () => import('@/views/tree/index'),
-        meta: { title: 'Tree', icon: 'tree' }
+        meta: { title: '树形', icon: 'tree' }
       }
     ]
   },
